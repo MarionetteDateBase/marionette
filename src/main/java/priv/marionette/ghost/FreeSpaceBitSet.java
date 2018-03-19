321bd10659b39585a2b0b31d876bf50b2a7c973c
Yue.Yu <Yue.Yu03@ele.me>
2018-03-19 18:47:18 +0800

    数据库数据存取核心模块

--- src/main/java/priv/marionette/ghost/FreeSpaceBitSet.java
+++ src/main/java/priv/marionette/ghost/FreeSpaceBitSet.java
@@ -1,10 +1,35 @@
 package priv.marionette.ghost;
 
+import java.util.BitSet;
+
 /**
- * 空间释放位图运算类
+ * 利用位图运算标记free space，用以垃圾回收释放空间
  *
  * @author Yue Yu
  * @create 2018-03-13 下午4:40
  **/
 public class FreeSpaceBitSet {
+    private static final boolean DETAILED_INFO = false;
+
+    /**
+     * 第一个有效数据块
+     */
+    private final int firstFreeBlock;
+
+    private final int blockSize;
+
+    /**
+     * The bit set.
+     */
+    private final BitSet set = new BitSet();
+    public FreeSpaceBitSet(int firstFreeBlock, int blockSize){
+        this.firstFreeBlock = firstFreeBlock;
+        this.blockSize = blockSize;
+        clear();
+    }
+
+    public void clear() {
+        set.clear();
+        set.set(0, firstFreeBlock);
+    }
 }
