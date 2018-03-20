18125d242ba216dbd7497d699aae53bea7c4b971
Yue.Yu <Yue.Yu03@ele.me>
2018-03-20 15:14:35 +0800

    数据库数据存取核心模块

--- src/main/java/priv/marionette/ghost/FreeSpaceBitSet.java
+++ src/main/java/priv/marionette/ghost/FreeSpaceBitSet.java
@@ -1,5 +1,7 @@
 package priv.marionette.ghost;
 
+import priv.marionette.tools.MathUtils;
+
 import java.util.BitSet;
 
 /**
@@ -19,7 +21,7 @@ public class FreeSpaceBitSet {
     private final int blockSize;
 
     /**
-     * The bit set.
+     * 位图
      */
     private final BitSet set = new BitSet();
     public FreeSpaceBitSet(int firstFreeBlock, int blockSize){
@@ -32,4 +34,174 @@ public class FreeSpaceBitSet {
         set.clear();
         set.set(0, firstFreeBlock);
     }
+
+    /**
+     * 检查从某个offset开始某一长度所包含的区块是否全部正在使用
+     * @param pos
+     * @param length
+     * @return
+     */
+    public boolean isUsed(long pos, int length) {
+        int start = getBlock(pos);
+        int blocks = getBlockCount(length);
+        for (int i = start; i < start + blocks; i++) {
+            if (!set.get(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 检查从某个offset开始某一长度所包含的区块是否全部等待释放
+     * @param pos
+     * @param length
+     * @return
+     */
+    public boolean isFree(long pos, int length) {
+        int start = getBlock(pos);
+        int blocks = getBlockCount(length);
+        for (int i = start; i < start + blocks; i++) {
+            if (set.get(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * 分配区块空间并标记正在使用
+     * @param length
+     * @return
+     */
+    public long allocate(int length) {
+        int blocks = getBlockCount(length);
+        for (int i = 0;;) {
+            int start = set.nextClearBit(i);
+            int end = set.nextSetBit(start + 1);
+            if (end < 0 || end - start >= blocks) {
+                set.set(start, start + blocks);
+                return getPos(start);
+            }
+            i = end;
+        }
+    }
+
+
+    /**
+     * 将指定区间强制标记为已使用
+     * @param pos
+     * @param length
+     */
+    public void markUsed(long pos, int length) {
+        int start = getBlock(pos);
+        int blocks = getBlockCount(length);
+        set.set(start, start + blocks);
+    }
+
+
+    /**
+     * 将指定空间强制释放
+     * @param pos
+     * @param length
+     */
+    public void free(long pos, int length) {
+        int start = getBlock(pos);
+        int blocks = getBlockCount(length);
+        set.clear(start, start + blocks);
+    }
+
+
+
+    private long getPos(int block) {
+        return (long) block * (long) blockSize;
+    }
+
+
+    private int getBlock(long pos) {
+        return (int) (pos / blockSize);
+    }
+
+    private int getBlockCount(int length) {
+        return MathUtils.roundUpInt(length, blockSize) / blockSize;
+    }
+
+
+    /**
+     * 计算当前空间的总体使用率
+     * @return
+     */
+    public int getFillRate() {
+        int total = set.length(), count = 0;
+        for (int i = 0; i < total; i++) {
+            if (set.get(i)) {
+                count++;
+            }
+        }
+        if (count == 0) {
+            return 0;
+        }
+        return Math.max(1, (int) (100L * count / total));
+    }
+
+    /**
+     * 获取第一个free空间的offset
+     * @return
+     */
+    public long getFirstFree() {
+        return getPos(set.nextClearBit(0));
+    }
+
+    /**
+     * 获取最后一个free空间的offset
+     * @return
+     */
+    public long getLastFree() {
+        return getPos(set.previousSetBit(set.size()-1) + 1);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder buff = new StringBuilder();
+        if (DETAILED_INFO) {
+            int onCount = 0, offCount = 0;
+            int on = 0;
+            for (int i = 0; i < set.length(); i++) {
+                if (set.get(i)) {
+                    onCount++;
+                    on++;
+                } else {
+                    offCount++;
+                }
+                if ((i & 1023) == 1023) {
+                    buff.append(String.format("%3x", on)).append(' ');
+                    on = 0;
+                }
+            }
+            buff.append('\n')
+                    .append(" on ").append(onCount).append(" off ").append(offCount)
+                    .append(' ').append(100 * onCount / (onCount+offCount)).append("% used ");
+        }
+        buff.append('[');
+        for (int i = 0;;) {
+            if (i > 0) {
+                buff.append(", ");
+            }
+            int start = set.nextClearBit(i);
+            buff.append(Integer.toHexString(start)).append('-');
+            int end = set.nextSetBit(start + 1);
+            if (end < 0) {
+                break;
+            }
+            buff.append(Integer.toHexString(end - 1));
+            i = end + 1;
+        }
+        buff.append(']');
+        return buff.toString();
+    }
+
+
+
+
 }
