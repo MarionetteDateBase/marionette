0cbae50a4201b2bb731238274d7d6551d1fce9fd
Yue.Yu <Yue.Yu03@ele.me>
2018-05-15 18:37:36 +0800

    数据库数据存取核心模块

--- src/main/java/priv/marionette/ghost/btree/Page.java
+++ src/main/java/priv/marionette/ghost/btree/Page.java
@@ -35,7 +35,7 @@ import static priv.marionette.tools.DataUtils.PAGE_TYPE_LEAF;
 public abstract class Page implements Cloneable{
 
 
-    private final MVMap<?, ?> map;
+    public  final MVMap<?, ?> map;
 
     private long pos;
 
@@ -161,7 +161,7 @@ public abstract class Page implements Cloneable{
         }
     }
 
-    private void addMemory(int mem) {
+    protected void addMemory(int mem) {
         memory += mem;
     }
 
@@ -191,7 +191,7 @@ public abstract class Page implements Cloneable{
         return mem;
     }
 
-    private void recalculateMemory() {
+    protected void recalculateMemory() {
         int mem = DataUtils.PAGE_MEMORY;
         DataType keyType = map.getKeyType();
         for (int i = 0; i < keys.length; i++) {
@@ -258,7 +258,7 @@ public abstract class Page implements Cloneable{
         }
     }
 
-    private boolean isPersistent() {
+    protected boolean isPersistent() {
         return memory != IN_MEMORY;
     }
 
