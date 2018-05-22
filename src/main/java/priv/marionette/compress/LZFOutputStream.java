abbfecc742ff643d8280438f41c4c94d3587df6e
Yue.Yu <Yue.Yu03@ele.me>
2018-05-22 16:18:05 +0800

    数据库数据存取核心模块

--- src/main/java/priv/marionette/compress/LZFOutputStream.java
+++ src/main/java/priv/marionette/compress/LZFOutputStream.java
@@ -1,8 +1,3 @@
-/*
- * Copyright 2004-2018 H2 Group. Multiple-Licensed under the MPL 2.0,
- * and the EPL 1.0 (http://h2database.com/html/license.html).
- * Initial Developer: H2 Group
- */
 package priv.marionette.compress;
 
 import priv.marionette.tools.Constants;
